75f6572861085667cb98970682933a84c089c582
pitomacAhmed <148368787+pitomacAhmed@users.noreply.github.com>
2023-12-25 16:38:47 +0100

    Add files via upload

---
+++ Nova zadaca/src/Nova.java
@@ -0,0 +1,23 @@
+
+public class Nova {
+
+	public static void main(String[] args) {
+		int x=1;
+		int y=1;
+		int z=1;
+	if (x>y || z>y);
+	    System.out.println("1"); 
+		
+		System.out.println("2");
+	if(Math.abs(x-y)>=z);
+	if(x>y)
+		System.out.println("3");
+	else 
+		System.out.println("4");
+	
+		System.out.println("5");
+	
+
+	}
+
+}
